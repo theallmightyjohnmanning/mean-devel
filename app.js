410c1264919550c50c545994128e1e7bd206ae9a
John Manning <john.manning89@gmail.com>
2016-05-16 15:46:00 -0400

    Setting up model/route/controller including structure

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 // Include the environment.json configuration file
-var fs      = require('fs');
-var config  = JSON.parse(fs.readFile("environment.json");
+var fs          = require('fs');
+var config      = JSON.parse(fs.readFileSync("environment.json"));
+
+// Instantiate database connection
+var mongoose    = require('mongoose');
+var db          = mongoose.connect('mongodb://'+config.db.host+':'+config.db.port+'/'+config.db.name);
+
+// Instantiate express
+var express     = require('express');
+var app         = express();
+
+// Instantiate body-parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Setup included directories
+app.use(express.static(__dirname+'/node_modules'));
+app.use(express.static(__dirname+'/public'));
+
+// Setup available models
+require('./models')(db);
+
+// Include Routes
+require('./routes');
+
+// Start server
+app.listen(config.server.port, config.server.host);
---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = function(db) {
+
+	require('./test')(db);
+}
